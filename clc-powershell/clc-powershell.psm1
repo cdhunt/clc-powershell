49a5947e2237d90431895bf2c50368aa6b33b07d
Chris Hunt <cdhunt@gmail.com>
2015-08-30 15:39:55 -0400

    Get-ClcGroup and Get-ClcServersFromGroup

--- clc-powershell/clc-powershell.psm1
+++ clc-powershell/clc-powershell.psm1
@@ -6,7 +6,7 @@ $VERSION = '/v2'
 #>
 function Get-ClcUri ( [string]$Path ) {
 	
-	$UriString = "$ROOT$VERSION"
+	$UriString = "$ROOT"
 
 	if (! [string]::IsNullOrEmpty($Path) )
 	{
@@ -59,7 +59,7 @@ function Get-ClcAuthenticationHeader {
 			password = $Credential.GetNetworkCredential().Password 
 		} | ConvertTo-Json)
 
-	[Uri]$uri = Get-ClcUri -Path 'authentication/login'
+	[Uri]$uri = Get-ClcUri -Path "$VERSION/authentication/login"
 	Write-Verbose $uri.AbsolutePath
 
 	$response = Invoke-RestMethod -Uri $uri -Method Post -ContentType 'application/json' -Body $body
@@ -68,4 +68,96 @@ function Get-ClcAuthenticationHeader {
 	$authHeader = @{Authorization = " Bearer " + $token}
 
 	$authHeader | Write-Output
+}
+
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Get-CLCGroup
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([PSCustomObject])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory, Position=0)]
+		[System.Collections.Hashtable]
+        $Authentication,
+
+        # Param2 help description
+		[Parameter(Mandatory, Position=1)]
+        [String]
+        $AccountAlias,
+
+		[Parameter(Mandatory, Position=2)]
+		[String]
+		$GroupId
+    )
+
+	$path = "$VERSION/groups/$AccountAlias/$GroupId"
+
+	$uri = Get-ClcUri -Path $path
+
+	$results = Invoke-RestMethod -Uri $uri -Method Get -Headers $Authentication -ContentType 'application/json' 
+
+	Write-Output -InputObject $results
+
+}
+
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Get-ClcServersFromGroup
+{
+    [CmdletBinding(DefaultParameterSetName='Default')]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(ValueFromPipeline, Position=0, ParameterSetname='Default')]
+        $InbputObject,
+
+        # Param2 help description
+		[Parameter(Mandatory, Position=0, ParameterSetname='Group')]
+        [String]
+        $AccountAlias,
+
+		[Parameter(Mandatory, Position=1, ParameterSetname='Group')]
+		[String]
+		$GroupId,
+
+        # Param1 help description
+        [Parameter(ValueFromPipeline, Position=1, ParameterSetname='Default')]
+		[Parameter(ValueFromPipeline, Position=2, ParameterSetname='Group')]
+        [System.Collections.Hashtable]
+        $Authentication
+    )
+
+	Process 
+	{
+		if ($PSBoundParameters.InbputObject)
+		{
+			$InbputObject.links | Where-Object {$_.rel -eq 'server'} | ForEach-Object {
+				$uri =  Get-ClcUri -Path $_.href
+				Invoke-RestMethod $uri -Method Get -ContentType 'application/json' -Headers $Authentication
+			}
+
+		}
+	}
 }
